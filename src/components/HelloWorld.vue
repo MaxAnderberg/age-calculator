180f07b9020e2ea4a5daf23acbf5cea4826522e1
Max <maxanderberg@gmail.com>
2023-08-27 12:13:58 +0200

    added poppins font

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -46,6 +46,7 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,500&family=Montserrat:wght@500;700&family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 h3 {
     margin: 40px 0 0;
 }
