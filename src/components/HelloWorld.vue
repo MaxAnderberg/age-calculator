949be6e9462f56e5dc2fef19aca9167434715979
Max <maxanderberg@gmail.com>
2023-09-04 14:08:41 +0200

    cleaned up unused css

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -125,24 +125,6 @@ export default {
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,500&family=Montserrat:wght@500;700&family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
-h3 {
-    margin: 40px 0 0;
-}
-
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-
-li {
-    display: inline-block;
-    margin: 0 10px;
-}
-
-a {
-    color: #42b983;
-}
-
 .main-age_card {
     background-color: white;
     border-radius: 24px 24px 100px 24px;
